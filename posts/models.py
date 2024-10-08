1ebe9f8ddcdfb5d9c775f110003e17b7b4842086
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-08 14:30:36 +0000

    added iamge_filter_choices field and image validation

---
+++ posts/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    """
+    Post model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    image_filter_choices = [
+        ('eczema_relief', 'Eczema Relief'), 
+        ('dry_skin', 'Dry Skin'),
+        ('flare_control', 'Flare Control'),
+        ('allergy_block', 'Allergy Block'),
+        ('soothing_care', 'Soothing Care'),
+        ('hydration_boost', 'Hydration Boost'),
+        ('anti_itch', 'Anti-Itch'),
+        ('rash_reducer', 'Rash Reducer'),
+        ('calming_effect', 'Calming Effect'),
+        ('moisture_lock', 'Moisture Lock'),
+        ('sensitive_skin', 'Sensitive Skin'),
+        ('allergy_defense', 'Allergy Defense'),
+        ('relief_plus', 'Relief Plus'),
+        ('barrier_protect', 'Barrier Protect'),
+        ('soothe_and_heal', 'Soothe & Heal'),
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_aczthl', blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
