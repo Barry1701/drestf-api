0131ba812f318250700e11bfe9d5b28001102296
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-18 08:04:41 +0000

    use cloudinary in profiles model in image

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 
 class Profile(models.Model):
@@ -9,8 +11,9 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='../default_profile_b0dubq'
+    image = CloudinaryField(
+        'image', 
+        default='https://res.cloudinary.com/dprwuhawr/image/upload/v1728385884/media/images/default_profile_b0dubq.jpg'
     )
 
     class Meta:
