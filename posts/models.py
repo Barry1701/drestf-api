d52b71588776b6ed7c9dc19b37f34cb81ff6da25
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-22 13:26:48 +0000

    add cloudinary to posts image

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tags.models import Tag
+from cloudinary.models import CloudinaryField
+
 
 
 class Post(models.Model):
@@ -30,8 +32,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='../default_post_aczthl', blank=True
+    image = CloudinaryField( 'image', default='../default_post_aczthl', blank=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
