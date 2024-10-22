245428c743fc48b8a8aaea1a749bb62188481a85
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-22 14:47:37 +0000

    add image.url to post serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -17,7 +17,8 @@ class PostSerializer(serializers.ModelSerializer):
         slug_field='name',
         queryset=Tag.objects.all()
     )
-    image = serializers.ImageField()
+    # Nowe pole do zwracania pełnego URL-a obrazu
+    image_url = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -44,6 +45,11 @@ class PostSerializer(serializers.ModelSerializer):
             ).first()
             return like.id if like else None
         return None
+    
+    def get_image_url(self, obj):
+        if obj.image:
+            return obj.image.url  # Pobierz pełny URL do obrazu z Cloudinary
+        return None
 
     class Meta:
         model = Post
@@ -52,5 +58,5 @@ class PostSerializer(serializers.ModelSerializer):
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
             'like_id', 'likes_count', 'comments_count',
-            'tags'
+            'tags', 'image_url'  # Dodajemy image_url
         ]
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,59 +2,32 @@ from rest_framework import serializers
 from .models import Profile
 from followers.models import Follower
 
-class PostSerializer(serializers.ModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    like_id = serializers.SerializerMethodField()
-    likes_count = serializers.ReadOnlyField()
-    comments_count = serializers.ReadOnlyField()
-    tags = serializers.SlugRelatedField(
-        many=True,
-        slug_field='name',
-        queryset=Tag.objects.all()
-    )
-    # Nowe pole do zwracania pełnego URL-a obrazu
-    image_url = serializers.SerializerMethodField()
-
-    def validate_image(self, value):
-        if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidationError('Image size larger than 2MB!')
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px!'
-            )
-        if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width larger than 4096px!'
-            )
-        return value
+    following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+    image = serializers.ImageField()  # Dodajemy pole image jako ImageField
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
-    def get_like_id(self, obj):
+    def get_following_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
-            like = Like.objects.filter(
-                owner=user, post=obj
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
             ).first()
-            return like.id if like else None
-        return None
-    
-    def get_image_url(self, obj):
-        if obj.image:
-            return obj.image.url  # Pobierz pełny URL do obrazu z Cloudinary
+            return following.id if following else None
         return None
 
     class Meta:
-        model = Post
+        model = Profile
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter',
-            'like_id', 'likes_count', 'comments_count',
-            'tags', 'image_url'  # Dodajemy image_url
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image', 'is_owner', 'following_id',
+            'posts_count', 'followers_count', 'following_count',
         ]
