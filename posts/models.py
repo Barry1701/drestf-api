7c1f1d03121ffe5d5dc74f5fe524a413ed214f57
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-22 15:02:19 +0000

    altered posts models

--- posts/models.py
+++ posts/models.py
@@ -33,7 +33,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_aczthl.jpg', blank=True
+        upload_to='images/', default='https://res.cloudinary.com/dprwuhawr/image/upload/v1728378396/default_post_aczthl.jpg', blank=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
