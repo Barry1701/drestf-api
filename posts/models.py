07ac6f65b77748e11fe1db841ff5cc67fe8309fc
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-25 06:56:14 +0000

    altered posts

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tags.models import Tag
-from cloudinary.models import CloudinaryField
 
 
 
@@ -34,7 +33,8 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    image = CloudinaryField('image', default='../default_post_aczthl', blank=True
+    image = models.ImageField(
+        upload_to='images/', default='../default_post__aczthl', blank=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
