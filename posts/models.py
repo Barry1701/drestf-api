21eb83d5550091fb8fcedf0938295111d4ca51f0
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-28 07:43:23 +0000

    altered posts and profiles models

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 
 
@@ -33,8 +35,8 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='../default_post_ehnhuw', blank=True
+    image = CloudinaryField(
+        'image', default='../default_post_ehnhuw', blank=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
