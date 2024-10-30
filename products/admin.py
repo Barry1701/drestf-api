a93cc4be9087f93963729ae0afe16effb7ced775
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-30 22:35:40 +0000

    registered products and categories in admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Category  # Dodaj Category, jeśli istnieje
 
-# Register your models here.
+# Zarejestruj oba modele w panelu admina
+admin.site.register(Product)
+admin.site.register(Category)
