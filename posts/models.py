3b2ee1ccf33b7d845047adb1de627b8e236bb2de
Bartosz Stanczuk <synergy170185@gmail.com>
2024-11-02 11:30:33 +0000

    altered back for ImageField due to internal error

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+
+
+
+
+
+
 
 class Post(models.Model):
     """
-    Post model, related to 'owner', i.e., a User instance.
+    Post model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
     """
     image_filter_choices = [
@@ -29,11 +34,13 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    # Using CloudinaryField for image handling
-    image = CloudinaryField('image', blank=True, null=True)
+    image = models.ImageField(
+        upload_to='images/', default='default_post_ehnhuw', blank=True
+    )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
     )
+
 
     class Meta:
         ordering = ['-created_at']
