c90ff5786749ca6edfa54c20255c601b05f4d367
Bartosz Stanczuk <synergy170185@gmail.com>
2024-11-26 17:51:57 +0000

    working on cors

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -132,7 +132,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 
 # if 'CLIENT_ORIGIN_DEV' in os.environ:
 #     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
