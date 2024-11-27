10692b6003ac4611d0b8bcdf035e8abad6d08f0c
Bartosz Stanczuk <synergy170185@gmail.com>
2024-11-27 11:29:08 +0000

    change debug for True for admin purposes

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEBUG' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
