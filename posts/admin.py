8edbb60548967ee61f044b230b54578063155033
Bartosz Stanczuk <synergy170185@gmail.com>
2024-12-27 10:09:47 +0000

    register post model in admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner', 'category', 'created_at')  # Kolumny w widoku listy
+    list_filter = ('category', 'created_at')  # Filtry boczne
+    search_fields = ('title', 'content', 'owner__username')  # Pole wyszukiwania
+    ordering = ('-created_at',)  # Domyślne sortowanie
